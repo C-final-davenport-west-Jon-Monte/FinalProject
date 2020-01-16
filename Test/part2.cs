690c89219966fde57025fcab417ff47fd3e0c33b
montavian <57112515+montavian@users.noreply.github.com>
2020-01-16 12:12:23 -0600

    update

---
+++ Test/part2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject
+{
+    class part2
+    {
+        public static void DisplayNew()
+        {
+
+            Console.WriteLine("Something to advance the story"  );
+        }
+    }
+}
